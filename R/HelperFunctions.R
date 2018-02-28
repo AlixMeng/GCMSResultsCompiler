cb29bdaef5361f3fc6c9ceeb4698c35ef2c86d21
Philip Bulsink <bulsinkp@gmail.com>
2018-02-28 15:32:48 -0500

    Finished moving functions to package positions

--- R/HelperFunctions.R
+++ R/HelperFunctions.R
@@ -64,118 +64,17 @@ element_count <- function(formula, element) {
     return(as.numeric(count))
 }
 
-V_element_count <- Vectorize(element_count, vectorize.args = c("formula"), SIMPLIFY = TRUE)
-
-
-#THIS LIKELY NEEDS REWORKING TO GET PROPER
-summarize_table <- function(ecount, ct){#} = compound_table) {
-    cut_table <- ct[ct$C == ecount[1] & ct$H == ecount[2] & ct$N == ecount[3] & ct$O == ecount[4] & ct$S == ecount[5] & ct$F ==
-        ecount[6] & ct$Cl == ecount[7] & ct$Br == ecount[8] & ct$I == ecount[9] & ct$P == ecount[10] & ct$B == ecount[11] &
-        ct$Si == ecount[12], ]
-    return(sum(as.numeric(cut_table$Area)))
-}
-
-# loop through the file list to read in data and clean it up
-dummyfunction<-function(filelist, directory, element_list, report_dir, compound_table){
-for (results_file in filelist) {
-
-    results <- as.data.frame(readxl::read_excel(paste0(directory, results_file), col_names = FALSE))
-
-    ctrow <- which(results[, 1] == "Compound Table")
-
-    header <- results[1:(ctrow - 1), ]
-    compound_table <- results[(ctrow + 1):nrow(results), ]
-
-    header <- Filter(function(x) !all(is.na(x)), header)
-    compound_table <- Filter(function(x) !all(is.na(x)), compound_table)
-
-    header_vals <- data.frame(key = character(), value = character())
-
-    for (i in seq(1, ncol(header), by = 2)) {
-        h <- unname(header[, i:(i + 1)])
-        h <- h[stats::complete.cases(h), ]
-        colnames(h) <- c("key", "value")
-        header_vals <- rbind(header_vals, h)
-    }
-
-    rm(header, h, results)
-
-    data_file_name <- header_vals[header_vals$key == "Data File", 2]
-    sample_id <- header_vals[header_vals$key == "Sample Name", 2]
-    sample_name <- header_vals[header_vals$key == "Comment", 2]
-    analysis_date <- header_vals[header_vals$key == "Acquired Time", 2]
-    acquisition_method <- header_vals[header_vals$key == "Acq Method", 2]
-    data_analysis_method <- header_vals[header_vals$key == "DA Method", 2]
-    operator_name <- header_vals[header_vals$key == "User Name", 2]
-    operator_name <- stringr::str_replace_all(operator_name, "\\\\", "_")  #four backslashes because escapes suck
-    operator_name <- stringr::str_replace_all(operator_name, "/", "_")
-
-    colnames(compound_table) <- compound_table[1, ]
-    compound_table <- compound_table[2:(nrow(compound_table) - 2), ]
-    compound_table <- compound_table[compound_table$RT != "RT", ]
-
-    for (i in 1:length(element_list)) {
-        compound_table[element_list[i]] <- V_element_count(formula = compound_table$`Molecular Formula`, element = element_list[i])
-    }
-
-    etable <- compound_table[, colnames(compound_table) %in% element_list]
-
-    total_area <- sum(as.numeric(compound_table$Area), na.rm = TRUE)
-
-    by_all <- unique(etable)
-
-    # Remove empty rows that sneak trhough sometimes
-    zeroRows <- unname((rowSums(by_all, na.rm = TRUE) == 0))
-    if (sum(zeroRows) > 0) {
-        by_all <- by_all[!zeroRows, ]
-    }
-
-    by_all$Area <- apply(by_all, 1, function(x) summarize_table(x, compound_table))
-    by_all$Area.Percent <- (by_all$Area/total_area) * 100
-
-    by_c <- data.frame(`Carbon Count` = integer(), Area = integer(), Area.Percent = numeric())
-    by_ch <- data.frame(`Carbon Count` = integer(), `Hydrogen Count` = integer(), Area = integer(), Area.Percent = numeric())
-
-    for (i in unique(compound_table$C)) {
-        ct <- compound_table[compound_table$C == i, ]
-        nr <- data.frame(`Carbon Count` = i, Area = sum(as.numeric(ct$Area)), Area.Percent = (sum(as.numeric(ct$Area))/total_area) *
-            100)
-        by_c <- rbind(by_c, nr)
-        for (j in unique(ct$H)) {
-            ch <- ct[ct$H == j, ]
-            nh <- data.frame(`Carbon Count` = i, `Hydrogen Count` = j, Area = sum(as.numeric(ch$Area)), Area.Percent = (sum(as.numeric(ch$Area))/total_area) *
-                100)
-            by_ch <- rbind(by_ch, nh)
-        }
-    }
-
-    by_c <- by_c[order(by_c$Carbon.Count), ]
-    by_ch <- by_ch[order(by_ch$Carbon.Count, by_ch$Hydrogen.Count), ]
-    by_all <- by_all[order(by_all$C, by_all$H, by_all$N, by_all$O, by_all$S, by_all$F, by_all$Cl, by_all$Br, by_all$I, by_all$P,
-        by_all$B, by_all$Si), ]
-
-    # Remove columns with elements that never show up.
-    zeroCols <- (colSums(by_all, na.rm = TRUE) == 0)
-    if (sum(zeroCols) > 0) {
-        by_all <- by_all[, !zeroCols]
-    }
-    element_included_list <- colnames(by_all)[1:(length(colnames(by_all)) - 2)]
-
-
-    rownames(by_all) <- NULL
-    rownames(by_c) <- NULL
-    rownames(by_ch) <- NULL
-
-    rmarkdown::render(input = system.file("rmd/genericreport.rmd", package = "GCMSResultsCompiler"), output_format = "pdf_document",
-        output_file = paste(sample_id, "_breakdown.pdf", sep = ""), output_dir = report_dir, params = list(set_title = paste0(sample_id,
-            " Breakdown Report"), set_date = Sys.Date()))
-
-    utils::write.csv(x = by_c, file = paste0(report_dir, "/", sample_id, "_by_c.csv"), row.names = FALSE)
-    message("Output created: ", paste0(report_dir, "/", sample_id, "_by_c.csv"))
-    utils::write.csv(x = by_ch, file = paste0(report_dir, "/", sample_id, "_by_ch.csv"), row.names = FALSE)
-    message("Output created: ", paste0(report_dir, "/", sample_id, "_by_ch.csv"))
-    utils::write.csv(x = by_all, file = paste0(report_dir, "/", sample_id, "_by_all.csv"), row.names = FALSE)
-    message("Output created: ", paste0(report_dir, "/", sample_id, "_by_all.csv"))
-
-}
+#' Extract Header Info
+#'
+#' Get values out of a data frame with key:value columns. Return the value if string, or stop() if not found
+#'
+#' @param info_frame The data frame, with two columns: \code{key} and \code{value}
+#' @param info_key The key from which to extract data, if it exists.
+#'
+#' @keywords Internal
+#'
+#' @return value from key:value pair
+extract_key_value <- function(info_frame, info_key){
+  stopifnot(info_key %in% info_frame$key, paste0(info_key, " not found in source file headers."))
+  return(info_frame[info_frame$key == info_key, ]$value)
 }
--- R/ReportCompiler.R
+++ R/ReportCompiler.R
@@ -31,8 +31,9 @@ compile_reports <- function(source_file_dir, results_files_dir = NULL, report_te
   element_list<-check_heteroatoms(heteroatoms)
 
   for (results_file in filelist){
+    source_file<-file.path(source_file_dir, results_file)
     tryCatch(
-      single_report(results_file, results_files_dir, report_template, element_list, write_pdf, write_csv, ...),
+      single_report(source_file, results_files_dir, report_template, element_list, write_pdf, write_csv, ...),
       error = function(e) message("Error in generating report file from ", results_file, "./nError message: ", e),
       warning = function(w) message("Warning in generating report file from", results_file, "./nWarning message: ", w)
       )
@@ -63,5 +64,133 @@ single_report<-function(results_file, results_files_dir, report_template, elemen
     knitverbose <- FALSE
   }
 
+  results <- as.data.frame(readxl::read_excel(results_file, col_names = FALSE))
+
+  ctrow <- which(results[, 1] == "Compound Table")
+  stopifnot(ctrow > 0, "Could not find Compound Table in source file.")
+
+  header <- results[1:(ctrow - 1), ]
+  compound_table <- results[(ctrow + 1):nrow(results), ]
+
+  header <- Filter(function(x) !all(is.na(x)), header)
+  compound_table <- Filter(function(x) !all(is.na(x)), compound_table)
+
+  header_vals <- data.frame(key = character(), value = character())
+
+
+  for (i in seq(1, ncol(header), by = 2)) {
+    h <- unname(header[, i:(i + 1)])
+    h <- h[stats::complete.cases(h), ]
+    colnames(h) <- c("key", "value")
+    header_vals <- rbind(header_vals, h)
+  }
+
+  rm(header, h, results)
+
+  data_file_name <- extract_key_value(header_vals, "Data File")
+  sample_id <- extract_key_value(header_vals, "Sample Name")
+  sample_name <- extract_key_value(header_vals, "Comment")
+  analysis_date <- extract_key_value(header_vals, "Acquired Time")
+  acquisition_method <- extract_key_value(header_vals, "Acq Method")
+  data_analysis_method <- extract_key_value(header_vals, "DA Method")
+  operator_name <- extract_key_value(header_vals, "User Name")
+
+  #fixing usernames to not mess up tex
+  operator_name <- stringr::str_replace_all(operator_name, "\\\\", "_")  #four backslashes because escapes suck
+  operator_name <- stringr::str_replace_all(operator_name, "/", "_")
+
+  colnames(compound_table) <- compound_table[1, ]
+  compound_table <- compound_table[2:(nrow(compound_table) - 2), ]
+  compound_table <- compound_table[compound_table$RT != "RT", ]
+
+  V_element_count <- Vectorize(element_count, vectorize.args = c("formula"), SIMPLIFY = TRUE)
+
+  for (i in 1:length(element_list)) {
+    compound_table[element_list[i]] <- V_element_count(formula = compound_table$`Molecular Formula`, element = element_list[i])
+  }
+
+  # Remove empty rows that sneak trhough sometimes
+  zeroRows <- unname((rowSums(by_all, na.rm = TRUE) == 0))
+  if (sum(zeroRows) > 0) {
+    by_all <- by_all[!zeroRows, ]
+  }
+
+  compound_table$Area <- as.numeric(compound_table$Area)
+  total_area <- sum(compound_table$Area, na.rm = TRUE)
+
+  # by_all <- compound_table %>%
+  #   dplyr::select(one_of(c(element_list, 'Area'))) %>%
+  #   dplyr::group_by(one_of(element_list)) %>%
+  #   dplyr::summarise(Area = sum(Area)) %>%
+  #   dplyr::mutate(Area.Percent = Area/total_area*100) %>%
+  #   dplyr::arrange(C, H, Area)
+  #
+  # by_c <- compound_table %>%
+  #   dplyr::select(one_of(c(element_list, 'Area'))) %>%
+  #   dplyr::group_by(C) %>%
+  #   dplyr::summarise(Area = sum(Area)) %>%
+  #   dplyr::mutate(Area.Percent = Area/total_area*100) %>%
+  #   dplyr::arrange(C)
+  #
+  # by_ch <- compound_table %>%
+  #   dplyr::select(one_of(c(element_list, 'Area'))) %>%
+  #   dplyr::group_by(C, H) %>%
+  #   dplyr::summarise(Area = sum(Area)) %>%
+  #   dplyr::mutate(Area.Percent = Area/total_area*100) %>%
+  #   dplyr::arrange(C, H)
+
+  #not great practice, but clears cran
+  C<-H<-Area<-NULL
+
+  by_c <- dplyr::select(compound_table, dplyr::one_of(c(element_list, 'Area')))
+  by_c <- dplyr::group_by(by_c, C)
+  by_c <- dplyr::summarise(by_c, Area = sum(Area))
+  by_c <- dplyr::mutate(by_c, Area.Percent = Area/total_area*100)
+  by_c <- dplyr::arrange(by_c, C)
+
+  by_ch <- dplyr::select(compound_table, dplyr::one_of(c(element_list, 'Area')))
+  by_ch <- dplyr::group_by(by_ch, C, H)
+  by_ch <- dplyr::summarise(by_ch, Area = sum(Area))
+  by_ch <- dplyr::mutate(by_ch, Area.Percent = Area/total_area*100)
+  by_ch <- dplyr::arrange(by_ch, C, H)
+
+  by_all <- dplyr::select(compound_table, dplyr::one_of(c(element_list, 'Area')))
+  by_all <- dplyr::group_by(dplyr::one_of(element_list))
+  by_all <- dplyr::summarise(by_all, Area = sum(Area))
+  by_all <- dplyr::mutate(by_all, Area.Percent = Area/total_area*100)
+  by_all <- dplyr::arrange(by_all, C, H, Area)
+
+  element_included_list <- colnames(by_all)[1:(length(colnames(by_all)) - 2)]
+
+  rownames(by_all) <- NULL
+  rownames(by_c) <- NULL
+  rownames(by_ch) <- NULL
+
+  if(write_pdf){
+    rmarkdown::render(input = system.file("rmd/genericreport.rmd", package = "GCMSResultsCompiler"), output_format = "pdf_document",
+                      output_file = paste(sample_id, "_breakdown.pdf", sep = ""), output_dir = results_files_dir,
+                      params = list(set_title = paste0(sample_id, " Breakdown Report"), set_date = Sys.Date()))
+    if(verbose==1){
+      message("PDF created: ", file.path(results_files_dir, paste0(sample_id, "_breakdown.csv")))
+    }
+  }
+
+  if(write_csv){
+    utils::write.csv(x = by_c, file = file.path(results_files_dir, paste0(sample_id, "_by_c.csv")), row.names = FALSE)
+    if(verbose == 2){
+      message("Output created: ", file.path(results_files_dir, paste0(sample_id, "_by_c.csv")))
+    }
+    utils::write.csv(x = by_ch, file = file.path(results_files_dir, paste0(sample_id, "_by_ch.csv")), row.names = FALSE)
+    if(verbose == 2){
+      message("Output created: ", file = file.path(results_files_dir, paste0(sample_id, "_by_ch.csv")))
+    }
+    utils::write.csv(x = by_all, file = file.path(results_files_dir, paste0(sample_id, "_by_all.csv")), row.names = FALSE)
+    if(verbose == 2){
+      message("Output created: ", file.path(results_files_dir, paste0(sample_id, "_by_all.csv")))
+    } else if (verbose == 1) {
+      message("CSV files created for sample: ", sample_id)
+    }
+
+  }
 
 }
